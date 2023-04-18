c9543ba5a947e4ab913c30f8041850f7e41fcbdb
Crhystel <crhystel.velasco@udla.edu.ec>
2023-04-17 23:04:02 -0500

    ejercicio 4

--- hola.c
+++ hola.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main(){
-    printf("hola");
+    int x,y, i;
+    printf("Ingrese dos numeros limites\n");
+    scanf("%d\n",&x);
+    scanf("%d",&y);
+    printf("Los numeros entre los limites son:\n");
+    for(i=x+1; i<=y-1; i++)
+    {
+        printf("%d\n", i);
+    }
+
     return 0;
 }
